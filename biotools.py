98a8ac5f1bc8f5b35a4af3f3c0e4a06cb2d54a10
Bruno Vellutini <organelas@gmail.com>
2012-07-06 11:26:27 +0200

    Transfer common fuctions to biotools library

---
+++ biotools.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''Common library for zim-biotools'''
+
+from datetime import datetime
+
+
+def make_header(title):
+    '''Return string with header of a Zim page.'''
+
+    timestamp = datetime.now().replace(microsecond=0)
+    header = '''Content-Type: text/x-zim-wiki\n
+                Wiki-Format: zim 0.4\n
+                Creation-Date: %s\n\n
+                ====== %s ======\n
+                Created %s\n\n''' % (timestamp.isoformat(), title, timestamp.strftime('%A %d %B %Y'))
+    return header
+
+
+def print_seq(sequence, page):
+    '''Print well-formatted sequence.'''
+    i = 0
+    for char in sequence:
+        if i == 60:
+            page.write('\n')
+            i = 0
+        page.write(char)
--- fastaimport.py
+++ fastaimport.py
@@ -11,30 +11,12 @@ Dependencies:
 
 import os
 import sys
-from datetime import datetime
 from Bio import SeqIO
 from Bio.Seq import translate
 
+from biotools import make_header, print_seq
+
 #FIXME If there are no subpages, create them.
-
-
-def make_header(title):
-    '''Return string with header of a Zim page.'''
-
-    timestamp = datetime.now().replace(microsecond=0)
-    header = '''Content-Type: text/x-zim-wiki\nWiki-Format: zim 0.4\nCreation-Date: %s\n\n====== %s ======\nCreated %s\n\n''' % (timestamp.isoformat(), title, timestamp.strftime('%A %d %B %Y'))
-    return header
-
-
-def print_seq(sequence, page):
-    '''Print well-formatted sequence.'''
-    i = 0
-    for char in sequence:
-        if i == 60:
-            page.write('\n')
-            i = 0
-        page.write(char)
-        i += 1
 
 
 def main(fasta_file, root_page):
