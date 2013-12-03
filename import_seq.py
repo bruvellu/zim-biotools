f255318f322b253fccc61c3e46c04c5b93a089d2
Bruno Vellutini <organelas@gmail.com>
2013-12-03 22:00:09 +0100

    Updates sequence importer to function again

--- import_seq.py
+++ import_seq.py
@@ -1,62 +1,63 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-'''Tool for importing sequence files in Zim.'''
+'''Import sequence files to Zim.
+    
+    This script takes all .seq files of a folder and creates a new Zim page for each individual sequence.
+
+    Why: used to import sequence files returned by our genome sequencing facilities.
+'''
 
 import os
 import sys
-from Bio import SeqIO
 from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
 
-from biotools import make_header
+from biolib import *
 
-#TODO Use SeqRecord as standard format for easy export.
-#TODO Print useful information such as length.
 
 def main(seq_dir, root_page):
-    # Remove extension.
-    root = root_page[:-4]
-    # Make sure there is a directory for sequences.
-    try:
-        os.mkdir(root)
-    except:
-        pass
-    # Get sequences from dir.
+
+    # Define subfolder by removing file extension from root page..
+    root = remove_extension(root_page)
+
+    # Make sure there is a directory for imported sequences.
+    make_dir(root)
+
+    # Get sequences from directory.
     sequences = os.listdir(seq_dir)
-    # Only filenames with .seq extension.
+
+    # Get only filenames with .seq extension.
     sequences = [seq for seq in sequences if seq.endswith('.seq')]
 
     # Iterate over each sequence file.
     for seq_filename in sequences:
 
-        sequence = open(os.path.join(seq_dir, seq_filename))
+        # Open sequence file for parsing.
+        seq_filepath = os.path.join(seq_dir, seq_filename)
+        sequence = open(seq_filepath)
 
-        # Parse filename as ID and ID as filename.
-        seq_id = os.path.basename(sequence.name.split('-')[0])
-        #seq_name
+        # New filepath for sequence file.
+        seq_basename = os.path.splitext(seq_filename)[0]
+        seq_basename = sanitize_zim(seq_basename)
+        seq_zimpath = os.path.join(root, '%s.txt' % seq_basename)
 
-        # Define locus file.
-        seq_file = os.path.join(root, '%s.txt' % seq_id)
-
-        # Verify if page already exists.
-        try:
-            seq_page = open(seq_file, 'r')
-            seq_page.close()
-            # If it does, skip page.
+        # If file already exists, skip sequence; never overwrite.
+        if file_exists(seq_zimpath):
             continue
-        except:
-            # If not, create page.
-            pass
+
+        # Write new file.
 
         # Create locus_page and write header.
-        seq_page = open(seq_file, 'w')
-        seq_page.write(make_header(os.path.basename(seq_id)))
+        seq_page = open(seq_zimpath, 'w')
+        seq_page.write(make_header(seq_basename))
 
-        # Write sequence in FASTA format.
-        seq_page.write('@sequence \n')
-        seq_page.write("'''\n")
-        seq_page.write(sequence.read())
-        seq_page.write("\n'''\n")
+        # Create SeqRecord for sequence.
+        seq_itself = sequence.read().replace('\r', '').replace('\n', '').replace('\t', '')
+        seq_record = SeqRecord(Seq(seq_itself), id=seq_basename, name=seq_basename, description='')
+
+        # Write sequence in fasta format.
+        seq_page.write(write_fasta(seq_record, tag='sequence'))
 
         # Close locus file.
         seq_page.close()
