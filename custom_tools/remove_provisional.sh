d53d1ec309ac0eb717e589015919cc446b81ec12
Bruno Vellutini <organelas@gmail.com>
2014-10-19 14:32:33 +0200

    Adds description

--- custom_tools/remove_provisional.sh
+++ custom_tools/remove_provisional.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Name: Remove provisional
+# Description: Removes notes with tag @provisional from current directory
+# Command: ~/.local/share/zim/plugins/zim-biotools/custom_tools/remove_provisional.sh %s
+
 ROOT=`dirname $1`
 
 grep -rl "@provisional" $ROOT | xargs rm
